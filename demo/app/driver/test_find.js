fc3fc4abd54e6e83f011f9159ca0c33595a905fc
Yi Zhao <zhaoyi0113@gmail.com>
2017-09-07 09:51:01 +1000

    refactor toarray

--- demo/app/driver/test_find.js
+++ demo/app/driver/test_find.js
@@ -1,10 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const esprima = require('esprima');
+const estraverse = require('estraverse');
+const escodegen = require('escodegen');
+
+const ast = esprima.parse('db.find()');
+let finished = false;
+estraverse.traverse(ast, {
+  leave: (node, parent) => {
+    if (node.type === esprima.Syntax.ExpressionStatement && !finished) {
+      finished = true;
+      let statement = escodegen.generate(node);
+      statement = `${statement.substring(0, statement.lastIndexOf(';'))}.toArray()`;
+      const findAst = esprima.parse(statement);
+      node.arguments = findAst.body[0].expression.arguments;
+      node.callee = findAst.body[0].expression.callee;
+      node.type = findAst.body[0].expression.type;
+    }
+  },
+});
+
+const generated = escodegen.generate(ast);
+console.log('generated  code:', generated);
+
 // Connection URL
 const url = 'mongodb://localhost:27017/SampleCollections';
 
-MongoClient.connect(url, async(err, db) => {
-  db.collection('explains').find({ 'user.name.last': 'Hall' }, { _id: 0 }).toArray().then((docs) => {
-    console.log(docs);
-  });
-});
+// MongoClient.connect(url, async(err, db) => {
+//   db.collection('explains').find({ 'user.name.last': 'Hall' }, { _id: 0 }).toArray().then((docs) => {
+//     console.log(docs);
+//   });
+// });
--- src/mongo-shell-translator.js
+++ src/mongo-shell-translator.js
@@ -5,23 +5,29 @@ const esprima = require('esprima');
 const estraverse = require('estraverse');
 const escodegen = require('escodegen');
 
+const parseOptions = {
+  tolerant: true,
+  raw: true,
+  tokens: true,
+  range: true,
+  comment: true,
+};
+
 class MongoShellTranslator {
 
+  constructor() {
+    this.statementType = '';
+  }
+
   translate(shell) {
-    const ast = esprima.parseScript(shell, {
-      tolerant: true,
-      raw: true,
-      tokens: true,
-      range: true,
-      comment: true,
-    });
+    const ast = esprima.parseScript(shell, parseOptions);
     estraverse.traverse(ast, {
       enter: (node, parent) => {
         if (node.type === esprima.Syntax.CallExpression) {
           const callee = node.callee;
           if (callee.type === esprima.Syntax.MemberExpression &&
             callee.property.name === 'find') {
-            node.mongoShellType = 'find';
+            this.statementType = 'find';
             let query;
             let fields;
             if (node.arguments.length > 0) {
@@ -36,23 +42,47 @@ class MongoShellTranslator {
             if (callee.object.type === esprima.Syntax.MemberExpression) {
               callee.object = createFindStatement(findDbName(node), callee.object.property.name);
             }
-            if (parent.type === esprima.Syntax.VariableDeclarator ||
-              parent.type === esprima.Syntax.ExpressionStatement) {
-              if (!node.property) {
-                node.type = esprima.Syntax.CallExpression;
-                node.callee = {
-                  type: esprima.Syntax.MemberExpression,
-                  object: { ...node },
-                  property: { type: esprima.Syntax.Identifier, name: 'toArray' },
-                };
-                node.arguments = [];
-                console.log('new node', node);
+            // if (parent.type === esprima.Syntax.VariableDeclarator ||
+            //   parent.type === esprima.Syntax.ExpressionStatement) {
+            //   if (!node.property) {
+            //     node.type = esprima.Syntax.CallExpression;
+            //     node.callee = {
+            //       type: esprima.Syntax.MemberExpression,
+            //       object: { ...node },
+            //       property: { type: esprima.Syntax.Identifier, name: 'toArray' },
+            //     };
+            //     node.arguments = [];
+            //   }
+            // }
+          }
+        }
+      },
+      leave: (node, parent) => {
+        if (node.type === esprima.Syntax.ExpressionStatement) {
+          if (this.statementType === 'find') {
+            this.statementType = '';
+            let statement = escodegen.generate(node);
+            console.log('statement = ', statement);
+            if (!statement.trim().endsWith('toArray();')) {
+              const idx = statement.lastIndexOf(';');
+              statement = `${statement.substring(0, idx)}.toArray();`;
+              const findAst = esprima.parse(statement, parseOptions);
+              console.log('origin node ', node);
+              console.log('new statement = ', statement);
+              console.log('find ast ', findAst);
+              if (findAst && findAst.type === esprima.Syntax.Program) {
+                if (findAst.body && findAst.body.length > 0) {
+                  node.expression.arguments = findAst.body[0].expression.arguments;
+                  node.expression.callee = findAst.body[0].expression.callee;
+                  node.expression.type = findAst.body[0].expression.type;
+                }
               }
+              console.log('new node ', node);
+              console.log('updated node');
             }
           }
         }
       },
-      leave: (node, parent) => {},
     });
     return generate(ast);
   }
