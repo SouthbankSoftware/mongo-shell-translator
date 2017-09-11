e9cb62e06d4e912a1c2e97811ffefa2401d594f2
Yi Zhao <zhaoyi0113@gmail.com>
2017-09-11 13:12:48 +1000

    update delete translator

--- src/mongo-shell-translator.js
+++ src/mongo-shell-translator.js
@@ -29,46 +29,56 @@ class MongoShellTranslator {
     const ast = esprima.parseScript(shell, parseOptions);
     estraverse.traverse(ast, {
       cursor: 0,
-      enter: (node, parent) => {
+      enter: (node) => {
         if (node.type === esprima.Syntax.CallExpression) {
           const callee = node.callee;
           if (callee.type === esprima.Syntax.MemberExpression) {
-            if (callee.property.name === commandName.find) {
+            const translator = translators[callee.property.name];
+            if (translator) {
               this.statementType = callee.property.name;
               if (callee.object.type === esprima.Syntax.MemberExpression) {
-                const statementObj = findTranslator.createCollectionStatement(node,
-                  commonTranslator.findDbName(node), callee.object.property.name);
-                callee.object = statementObj.object;
-                node.arguments = statementObj.arguments;
-              }
-            } else if (callee.property.name === commandName.aggregate) {
-              this.statementType = commandName.aggregate;
-              if (callee.object.type === esprima.Syntax.MemberExpression) {
-                const statementObj = commonTranslator.createCollectionStatement(node,
-                  commonTranslator.findDbName(node), callee.object.property.name);
-                callee.object = statementObj.object;
-                node.arguments = statementObj.arguments;
-              }
-            } else if (callee.property.name === commandName.update ||
-              callee.property.name === commandName.updateOne ||
-              callee.property.name === commandName.updateMany) {
-              this.statementType = callee.property.name;
-              if (callee.object.type === esprima.Syntax.MemberExpression) {
-                const statementObj = updateTranslator.createCollectionStatement(node,
-                  commonTranslator.findDbName(node), callee.object.property.name, parent);
-                callee.object = statementObj.object;
-                node.arguments = statementObj.arguments;
-              }
-            } else if (callee.property.name === commandName.deleteOne ||
-              callee.property.name === commandName.deleteMany) {
-              this.statementType = callee.property.name;
-              if (callee.object.type === esprima.Syntax.MemberExpression) {
-                const statementObj = deleteTranslator.createCollectionStatement(node,
+                const statementObj = translator.createCollectionStatement(node,
                   commonTranslator.findDbName(node), callee.object.property.name);
                 callee.object = statementObj.object;
                 node.arguments = statementObj.arguments;
               }
             }
+            // if (callee.property.name === commandName.find) {
+            //   this.statementType = callee.property.name;
+            //   if (callee.object.type === esprima.Syntax.MemberExpression) {
+            //     const statementObj = findTranslator.createCollectionStatement(node,
+            //       commonTranslator.findDbName(node), callee.object.property.name);
+            //     callee.object = statementObj.object;
+            //     node.arguments = statementObj.arguments;
+            //   }
+            // } else if (callee.property.name === commandName.aggregate) {
+            //   this.statementType = commandName.aggregate;
+            //   if (callee.object.type === esprima.Syntax.MemberExpression) {
+            //     const statementObj = commonTranslator.createCollectionStatement(node,
+            //       commonTranslator.findDbName(node), callee.object.property.name);
+            //     callee.object = statementObj.object;
+            //     node.arguments = statementObj.arguments;
+            //   }
+            // } else if (callee.property.name === commandName.update ||
+            //   callee.property.name === commandName.updateOne ||
+            //   callee.property.name === commandName.updateMany) {
+            //   this.statementType = callee.property.name;
+            //   if (callee.object.type === esprima.Syntax.MemberExpression) {
+            //     const statementObj = updateTranslator.createCollectionStatement(node,
+            //       commonTranslator.findDbName(node), callee.object.property.name, parent);
+            //     callee.object = statementObj.object;
+            //     node.arguments = statementObj.arguments;
+            //   }
+            // } else if (callee.property.name === commandName.deleteOne ||
+            //   callee.property.name === commandName.deleteMany) {
+            //   this.statementType = callee.property.name;
+            //   if (callee.object.type === esprima.Syntax.MemberExpression) {
+            //     const statementObj = deleteTranslator.createCollectionStatement(node,
+            //       commonTranslator.findDbName(node), callee.object.property.name);
+            //     callee.object = statementObj.object;
+            //     node.arguments = statementObj.arguments;
+            //   }
+            // }
           }
         }
       },
@@ -77,15 +87,17 @@ class MongoShellTranslator {
           node.type === esprima.Syntax.VariableDeclarator ||
           node.type === esprima.Syntax.AssignmentExpression) {
           if (this.statementType === commandName.find ||
-            this.statementType === commandName.aggregate ||
-            this.statementType === commandName.deleteMany ||
-            this.statementType === commandName.deleteOne
+            this.statementType === commandName.aggregate
           ) {
             this.statementType = '';
             commonTranslator.addCallbackOnStatement(node, this.sType);
           } else if (this.statementType === commandName.updateOne ||
             this.statementType === commandName.updateMany ||
             this.statementType === commandName.update) {
+            this.statementType = '';
+            commonTranslator.addCallbackOnStatement(node, this.sType, false, true, true, 'r');
+          } else if (this.statementType === commandName.deleteMany ||
+            this.statementType === commandName.deleteOne) {
             this.statementType = '';
             commonTranslator.addCallbackOnStatement(node, this.sType, false, true, true, 'r');
           }
--- tests/delete-translator.test.js
+++ tests/delete-translator.test.js
@@ -5,10 +5,10 @@ const MongoShellTranslator = require('../src/mongo-shell-translator').MongoShell
 const options = require('../src/options');
 const utils = require('./utils');
 
-describe('test update translator', () => {
-  it('test update callback', () => {
+describe('test delete translator', () => {
+  it('test delete callback', () => {
     const translator = new MongoShellTranslator();
     const nativeCode = translator.translate('db.test.deleteOne()');
-    utils.assertStatementEqual(nativeCode, 'db.collection(\'test\').deleteOne({},{},function (err, r) {});');
+    utils.assertStatementEqual(nativeCode, 'db.collection(\'test\').deleteOne({},function (err, r) {});');
   });
 });
