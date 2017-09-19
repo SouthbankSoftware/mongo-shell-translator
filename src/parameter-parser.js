9cd979fd134f35691edf9e8e117e352c25139949
Yi Zhao <zhaoyi0113@gmail.com>
2017-09-19 12:07:41 +1000

    add nested parser

--- src/parameter-parser.js
+++ src/parameter-parser.js
@@ -1,21 +1,28 @@
 const esprima = require('esprima');
 
+const parseObjectExpressionArgument = (arg) => {
+  let queryObject = '{';
+  arg.properties.forEach((property, i) => {
+    if (property.value.type === esprima.Syntax.Literal) {
+      queryObject += `${property.key.raw}: q.${property.key.value}`;
+      if (i < arg.properties.length - 1) {
+        queryObject += ',';
+      }
+    } else if (property.value.type === esprima.Syntax.ObjectExpression) {
+      queryObject += `${property.key.raw}: ${parseObjectExpressionArgument(property.value)}`;
+    }
+  });
+  queryObject += '}';
+  return queryObject;
+};
+
 const parseQueryParameters = (arg) => {
   let queryObject = '';
   if (!arg) {
     return queryObject;
   }
   if (arg.type === esprima.Syntax.ObjectExpression) {
-    queryObject += '{';
-    arg.properties.forEach((property, i) => {
-      if (property.value.type === esprima.Syntax.Literal) {
-        queryObject += `${property.key.raw}: q.${property.key.value}`;
-        if (i < arg.properties.length - 1) {
-          queryObject += ',';
-        }
-      }
-    });
-    queryObject += '}';
+    queryObject = parseObjectExpressionArgument(arg);
   }
   return queryObject;
 };
--- tests/argument-parser.test.js
+++ tests/argument-parser.test.js
@@ -22,9 +22,21 @@ describe('argument parser test suite', () => {
     let qCode = parameterParser.parseQueryParameters(expression.arguments[0]);
     assert.equal(qCode, '{"name": q.name}');
 
-    code = esprima.parseScript('db.test.find({})');
+    code = esprima.parseScript('db.test.find({"first": "Joey", "last": "Zhao"})');
     expression = translator.findSupportedStatement(code.body[0]).expression;
     qCode = parameterParser.parseQueryParameters(expression.arguments[0]);
-    assert.equal(qCode, '{}');
+    assert.equal(qCode, '{"first": q.first,"last": q.last}');
+  });
+
+  it('test parse nested json query parameters', () => {
+    let code = esprima.parseScript('db.test.find({"name": "Joey", "full-name": {"last": "zhao", "first": "yi"}})');
+    let { expression } = translator.findSupportedStatement(code.body[0]);
+    let qCode = parameterParser.parseQueryParameters(expression.arguments[0]);
+    assert.equal(qCode, '{"name": q.name,"full-name": {"last": q.last,"first": q.first}}');
+
+    code = esprima.parseScript('db.test.find({"age": 100, "nested1": {"key1": 1, "nested2": {"key2": 2, "nested3": {"key3": "key3"}}}})');
+    expression = translator.findSupportedStatement(code.body[0]).expression;
+    qCode = parameterParser.parseQueryParameters(expression.arguments[0]);
+    assert.equal(qCode, '{"age": q.age,"nested1": {"key1": q.key1,"nested2": {"key2": q.key2,"nested3": {"key3": q.key3}}}}');
   });
 });
